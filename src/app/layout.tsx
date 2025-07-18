5ed6f634f68b7139fb5ce41cd3e49d80385c5a1d
ImxYJL <allensain14@gmail.com>
2025-07-18 14:32:57 +0900

    feat: 기본 페이지 및 레이아웃

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
-"use client";
+'use client';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/globals.css';
+import { pretendard } from './fonts/pretendard';
+import { QueryProvider } from '@/frontend/providers';
+import startMockWorker from '@/libs/msw/startMockWorker';
+
+startMockWorker();
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
-      <body>{children}</body>
+    <html lang="ko" className={pretendard.variable}>
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
