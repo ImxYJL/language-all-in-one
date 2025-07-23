282496301633891967d192435df453a6172f40f0
ImxYJL <allensain14@gmail.com>
2025-07-23 22:49:00 +0900

    feat: superbase DB 연동

--- src/libs/supabase/client.ts
+++ src/libs/supabase/client.ts
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { serverEnv } from '@/validators/env';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(serverEnv.SUPABASE_URL, serverEnv.SUPABASE_ANON_KEY);
