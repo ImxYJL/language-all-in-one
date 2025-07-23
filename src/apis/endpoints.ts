f43e89dc3c8440b58f38f5ed6ac6f8812d03612a
ImxYJL <allensain14@gmail.com>
2025-07-23 22:45:24 +0900

    refactor: 프론트가 사용하는 api 엔드포인트에서 baseurl 제거 - 순수 상대 경로만 사용

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -1,6 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? '/api' : `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`;
-
 export const ENDPOINT = {
-  login: `${BASE_URL}/login`,
-  getUserProfile: `${BASE_URL}/user/me/profile`,
-};
+  login: `/login`,
+  getUserProfile: `/user/me/profile`,
+} as const;
