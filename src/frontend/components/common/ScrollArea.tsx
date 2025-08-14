8ea96829f9ea1e6429e1c32ee6b684212370a304
ImxYJL <allensain14@gmail.com>
2025-08-14 14:52:06 +0900

    feat: 채팅방 내용이 있는 경우 스크롤을 하단으로 이동하는 기능 추가

--- src/frontend/components/common/ScrollArea.tsx
+++ src/frontend/components/common/ScrollArea.tsx
@@ -4,20 +4,28 @@ import { useScrollSensor } from '@/frontend/hooks';
 import { useEffect } from 'react';
 
 interface ScrollAreaProps {
+  currentChatId?: number;
   messages?: string[]; // 추후 변경 가능
   children: React.ReactNode;
   className?: string;
 }
 
-const ScrollArea = ({ messages, children, className }: ScrollAreaProps) => {
+const ScrollArea = ({ currentChatId, messages, children, className }: ScrollAreaProps) => {
   const { containerRef, isAtBottom, scrollToBottom } = useScrollSensor<HTMLDivElement>();
 
-  // 새 메시지가 오면 자동 스크롤
+  // 새 메시지가 오면 자동 스크롤 (조건으로 message 다는 게 맞는지 확인하기)
   useEffect(() => {
     if (messages && isAtBottom) {
       scrollToBottom(true);
     }
   }, [messages, isAtBottom]);
+
+  // 채팅방 내용이 있으면 하단으로 바로 이동
+  useEffect(() => {
+    if (messages && messages.length > 0) {
+      scrollToBottom(false);
+    }
+  }, [messages, currentChatId]);
 
   return (
     <div
