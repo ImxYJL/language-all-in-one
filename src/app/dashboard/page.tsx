2736a100fd634ad89deb7d739ec5299a4b149c86
ImxYJL <allensain14@gmail.com>
2025-08-26 14:46:17 +0900

    chore: 하이드레이션 테스트 코드 삭제

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,23 +21,12 @@ export default async function DashboardPage() {
     },
   });
 
-  // TODO: 토큰 제거
-  const cookieData = await cookies();
-  const token = cookieData.get('token')?.value;
-
-  // await queryClient.prefetchQuery({
-  //   queryKey: [QUERY_KEY.getUserInfo],
-  //   queryFn: () => getUserInfo(token),
-  // });
-
   const dehydratedState = dehydrate(queryClient);
-  console.log('dehydratedState', dehydratedState);
 
   return (
     <QueryProvider dehydratedState={dehydratedState}>
       <div className="p-6">
         <h1 className="mb-4 text-xl font-bold">테스트 하이드레이션 페이지</h1>
-        {/* <UserInfo /> */}
       </div>
     </QueryProvider>
   );
