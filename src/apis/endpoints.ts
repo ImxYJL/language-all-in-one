5914c7491f4e38ada9f1d757f5c4be1a167b46b4
ImxYJL <allensain14@gmail.com>
2025-09-15 23:19:45 +0900

    feat: 단어장 목록 엔드포인트

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -1,4 +1,29 @@
+import { ItemType } from '@/types/vocabulary';
+import { GetVocaListParams } from './vocabulary';
+
 export const ENDPOINT = {
   login: '/login',
   getRandomWord: '/wordnik',
+  getWords: ({ isFavorited, afterId, limit }: GetVocaListParams) =>
+    buildGetVocaListUrl({ isFavorited, afterId, limit, itemType: 'word' }),
+  getSentences: ({ isFavorited, afterId, limit }: GetVocaListParams) =>
+    buildGetVocaListUrl({ isFavorited, afterId, limit, itemType: 'sentence' }),
 } as const;
+
+export function buildGetVocaListUrl({
+  itemType,
+  isFavorited,
+  afterId,
+  limit = 20,
+}: GetVocaListParams & {
+  itemType: ItemType;
+}) {
+  const searchParams = new URLSearchParams();
+
+  if (limit) searchParams.set('limit', limit.toString());
+  if (afterId) searchParams.set('afterId', afterId);
+  if (isFavorited !== undefined) searchParams.set('favorited', isFavorited ? 'true' : 'false');
+
+  const queryString = searchParams.toString();
+  return queryString ? `/vocab/${itemType}s?${queryString}` : `/vocab/${itemType}s`;
+}
