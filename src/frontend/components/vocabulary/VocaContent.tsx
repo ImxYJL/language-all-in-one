4d27819e3382a64793d6f2621e1d22fdf47c69f9
ImxYJL <allensain14@gmail.com>
2025-09-22 21:48:15 +0900

    fix: 단어장 하단바를 사이드바 이동에 따라 함께 밀려나도록 변경

--- src/frontend/components/vocabulary/VocaContent.tsx
+++ src/frontend/components/vocabulary/VocaContent.tsx
@@ -22,7 +22,7 @@ const VocaContent = () => {
   const toggleIsFavorite = () => setIsFavorite((prev) => !prev);
 
   return (
-    <div className="w-full">
+    <div>
       <TabList isWordTab={isWordTab} toggleTab={toggleTab} />
 
       <VocaList isFavorite={isFavorite} isWordTab={isWordTab} />
@@ -31,14 +31,14 @@ const VocaContent = () => {
         {isWordTab ? <WordCreateForm closeSheet={toggleSheet} /> : <SentenceCreateForm closeSheet={toggleSheet} />}
       </BottomSheet>
 
-      <div className="fixed right-0 bottom-0 left-0 z-5 border border-gray-300 bg-white">
+      <div className="fixed right-0 bottom-0 left-0 z-5 border border-gray-300 bg-white transition-[left] duration-300 ease-in-out lg:group-data-[sidebar=open]/sidebar:left-80">
         <section className="grid h-16 grid-cols-3">
           <button
             type="button"
             onClick={toggleIsFavorite}
             className="text-primary flex flex-col items-center justify-center gap-1"
           >
-            즐겨찾기
+            {isFavorite === true ? '전체보기' : '즐겨찾기만 보기'}
           </button>
           <button
             type="button"
