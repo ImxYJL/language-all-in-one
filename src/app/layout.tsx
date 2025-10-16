2a349e8565bc49aeb4fb75b5cf8ed6dc36f8bcd0
ImxYJL <allensain14@gmail.com>
2025-10-16 17:40:11 +0900

    feat: 기존 기능(로그인, 단어장 요소 추가)에 토스트 알림 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/app/globals.css';
 import { pretendard } from './fonts/pretendard';
 import { QueryProvider } from '@/frontend/providers';
 import startMockWorker from '@/libs/msw/startMockWorker';
+import { ToastContainer } from 'react-toastify';
 // import { useEffect } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko" className={pretendard.variable}>
       <body>
+        <ToastContainer autoClose={2500} />
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
--- src/frontend/queries/auth/useLogin.ts
+++ src/frontend/queries/auth/useLogin.ts
@@ -1,9 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { login } from '@/apis/auth';
 import { LoginRequestBody } from '@/types/auth';
+import { toast } from 'react-toastify';
+import { getAxiosMessage } from '@/libs/axios/utils';
 
 export const useLogin = () => {
   return useMutation({
-    mutationFn: ({username, password}: LoginRequestBody) => login({username, password}),
+    mutationFn: ({ username, password }: LoginRequestBody) => login({ username, password }),
+    throwOnError: false,
+    onError: (e: Error) =>
+      toast.error(getAxiosMessage(e), {
+        position: 'top-center',
+      }),
   });
 };
