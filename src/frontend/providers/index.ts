9d1bbdd497b206ecbc86714bfb8db81fd1a2b467
ImxYJL <allensain14@gmail.com>
2025-10-17 23:05:47 +0900

    refactor: client component로 작동하는 provider를 하나의 ClientProvider로 묶음 - Provider의 children으로 server component 전달 가능

--- src/frontend/providers/index.ts
+++ src/frontend/providers/index.ts
@@ -2,3 +2,5 @@ export { default as QueryProvider } from './QueryProvider';
 export { default as ReactQueryDevtoolsProvider } from './ReactQueryDevtoolsProvider';
 export { ChatPageStateProvider } from './ChatPageStateProvider';
 export { SidebarStateProvider } from './SidebarStateProvider';
+export { default as QueryErrorProvider } from './QueryErrorProvider';
+export { default as ClientProviders } from './ClientProviders';
