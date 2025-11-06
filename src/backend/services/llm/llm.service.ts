44519ef01b0549e09d774d17b2783b88658ca40a
ImxYJL <allensain14@gmail.com>
2025-11-06 19:07:06 +0900

    refactor: 대화 생성과 이미 생성된 대화를 찾는 로직 분리 - id 유무로 대화방의 존재를 판단하는 것은 라우터의 역할로 내려감

--- src/backend/services/llm/llm.service.ts
+++ src/backend/services/llm/llm.service.ts
@@ -2,31 +2,37 @@ import { BaseLlm, ConversationMessage } from '@/backend/clients/llm/baseLlm';
 import { PROMPT, SUMMARY_INPUT } from '@/backend/clients/llm/gemini/gemini.constants';
 import { AppError, isNonExist, isUpstreamError } from '@/backend/error';
 import { SupabaseClient } from '@supabase/supabase-js';
-import { Message } from '@/backend/models/llm/types';
-import {
-  createConversation,
-  createMessage,
-  findConversationById,
-  getMessages,
-} from '@/backend/models/llm/gemini/conversation.model';
+import { Conversation, Message } from '@/backend/models/llm/types';
+import { createMessage, findConversationById, getMessages } from '@/backend/models/llm/gemini/conversation.model';
 
-async function getConversationId(db: SupabaseClient, userId: string, id: string): Promise<string> {
+export async function getValidConversation(db: SupabaseClient, userId: string, id: string): Promise<Conversation> {
   try {
     const chat = await findConversationById(db, id);
     if (chat.user_id !== userId) {
       throw AppError.forbidden(undefined, '접근 권한이 없습니다.');
     }
 
-    return chat.id;
+    return chat;
   } catch (err) {
-    if (isNonExist(err)) {
-      throw AppError.notFound(undefined, `${id}라는 대화를 찾을 수 없습니다.`);
-    }
+    if (isNonExist(err)) throw AppError.notFound(undefined, `Chat not found with id: ${id}`);
     throw err;
   }
 }
 
-export async function getConversations() {}
+async function getConversationId(db: SupabaseClient, userId: string, id: string): Promise<string> {
+  try {
+    const chat = await findConversationById(db, id);
+    if (chat.user_id !== userId) throw AppError.forbidden(undefined, '접근 권한이 없습니다.');
+
+    return chat.id;
+  } catch (err) {
+    if (isNonExist(err)) throw AppError.notFound(undefined, `${id}라는 대화를 찾을 수 없습니다.`);
+
+    throw err;
+  }
+}
+
+export async function getConversations(db: SupabaseClient, userId: string) {}
 
 function convertToContext(dbMessages: Message[]): ConversationMessage[] {
   return dbMessages.map((msg) => ({
@@ -58,32 +64,24 @@ export async function getMessageStream(
   db: SupabaseClient,
   llm: BaseLlm,
   input: string,
-  userId: string,
-  id?: string,
+  conversation: Conversation,
 ): Promise<LlmMessageStream> {
-  const conversationId = id ? await getConversationId(db, userId, id) : undefined;
+  const conversationId = conversation.id;
 
   // 컨텍스트 준비 및 스트림 생성
-  const dbMessages = conversationId ? await getMessages(db, conversationId) : [];
+  const dbMessages = await getMessages(db, conversationId);
   const contextMessages = convertToContext(dbMessages);
 
   const conversationPrompt = input === SUMMARY_INPUT ? PROMPT.summary : undefined;
 
   // 현재 유저가 입력한 메세지를 새 컨텍스트에 추가 (LLM 전달용)
-  // 아니잠만ㅋㅋㅋㅋㅋㅋ 얘는 DB입력 따라가서 DB타입 맞춰야하네...
-
-  // [현재 로직 정리] chat에는 꼭 추상화된 타입 써야 함. (추상화 + role과 content만 있음)
-  // 근데 db에서 갓 가져온 메세지 타입은 메타데이터(id 등)이 붙어 있음.
-  // 그래서 db -> 추상화 타입 변환된 contextMessages -> 여기다 유저 입력 push -> 이걸로 chat 넣어야 함.
   contextMessages.push({
     role: 'user',
     content: input,
   });
 
   const stream = createLlmStream(llm, contextMessages, conversationPrompt);
+  await createMessage(db, conversationId, 'user', input); // DB 저장용 유저 입력 저장
 
-  const finalConversationId = conversationId ?? (await createConversation(db, userId)).id;
-  await createMessage(db, finalConversationId, 'user', input); // DB 저장용 유저 입력 저장
-
-  return { stream, conversationId: finalConversationId };
+  return { stream, conversationId };
 }
